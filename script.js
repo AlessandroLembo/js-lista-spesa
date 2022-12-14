213740e9646fba0d87a89941f8f9aed6dd2d8ef6
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-14 15:32:13 +0100

    Analisi

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log(' JS OK');
+
+/*
+TRACCIA:
+
+Data una lista della spesa (inventatela), 
+stampare in pagina gli elementi della lista individualmente con un ciclo while.
+*/
+
+/*
+ANALISI:
+
+1 - Creo un array contenente una lista della spesa.
+2 - Prendo l'elemento dalla pagina.
+3 - Finchè ho elmenti nella lista li stampo in pagina 
+*/
+
+// 1 - Creo un array contenente una lista della spesa.
