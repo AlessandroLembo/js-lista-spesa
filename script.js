c32ecc79e5a10a79f937e6556b4f0f1ee23a8568
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-14 17:35:05 +0100

    modifica analisi e stampa in pagina

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ ANALISI:
 
 1 - Creo un array contenente una lista della spesa.
 2 - Prendo l'elemento dalla pagina.
-3 - Finchè ho elmenti nella lista li stampo in pagina 
+3 - Creo due variabili: una mi serve per contare gli elementi, l'altra per creare una lista.
+4 - Finchè ho elmenti nella lista li raccolgo per inserirli nella borsa.
+5 - Chiusura della lista.
+6 - Stampa in pagina.
 */
 
 // 1 - Creo un array contenente una lista della spesa.
@@ -23,14 +26,21 @@ const listElement = ['pane', 'latte', 'uova', 'sale', 'zucchero'];
 const bagElement = document.getElementById('bag');
 // console.log(bagElement);
 
-// 3 - Finchè ho elmenti nella lista li stampo in pagina.
+// 3 - Creazione due variabili.
 let i = 0;
-let currentElement = ' ';
+let currentElement = `<ul>`;
+
+// 4 - Finchè ho elmenti nella lista li raccolgo per inserirli nella borsa.
 while (i < listElement.length) {
-    console.log(listElement[i]);
-    currentElement += listElement[i];
-    // bagElement.innerText += listElement[i];
+    // console.log(listElement[i]);
+    currentElement += `<li>${listElement[i]}</li>`;
+    
     i++;
 }
 
-bagElement.innerText = currentElement;
+// 5 - Chiusura lista.
+currentElement += `</ul>`;
+
+// 6 - Stampa in pagina.
+console.log(currentElement);
+bagElement.innerHTML = currentElement;
