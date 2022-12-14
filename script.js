29070b85eaad438db6ee75c991121e8996556bc6
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-14 15:57:25 +0100

    ciclo while e stampa in pagina

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ ANALISI:
 */
 
 // 1 - Creo un array contenente una lista della spesa.
+const listElement = ['pane', 'latte', 'uova', 'sale', 'zucchero'];
+// console.log(listElement);
+
+// 2 - Prendo l'elemento dalla pagina.
+const bagElement = document.getElementById('bag');
+// console.log(bagElement);
+
+// 3 - Finchè ho elmenti nella lista li stampo in pagina.
+let i = 0;
+let currentElement = ' ';
+while (i < listElement.length) {
+    console.log(listElement[i]);
+    currentElement += listElement[i];
+    // bagElement.innerText += listElement[i];
+    i++;
+}
+
+bagElement.innerText = currentElement;
